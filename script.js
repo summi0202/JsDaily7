6d25ffe348650191d636e70ae7959dd0e76d9254
summi0202 <141727752+summi0202@users.noreply.github.com>
2024-01-08 00:33:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//Q1. What is the syntax for the While loop in JavaScript?
+
+// while (condition) {
+  // code to be executed while the condition is true
+// }
+ 
+//  Q2. What is the syntax for the For loop in JavaScript?
+// for (initialization; condition; afterthought)
+//   statement
+
+
+
+//Q3. What is a nested loop in JavaScript, and why would you use one?
+// A nested loop is a loop inside another loop. Although all kinds of loops can be nested, the most common nested loop involves for loops.
+
+//Use:- Nested loop is commonly used to iterate over two or more arrays or objects simultaneously.
+
+//Q4. What is an infinite loop in JavaScript, and how can it be avoided?
+
+// An infinite loop in JavaScript is a code construct that repeatedly executes a specific section of code indefinitely, leading to the program being stuck in an endless loop.
+// To avoid this issue, steps can be taken to properly define your conditions, break statements, and ensure your code design includes proper working logic.
+
+//Q5. How do you break out of two nested for loops?
+// There are several ways to break out of two nested for loops, depending on our programming language and specific loop structure. One common method is to use a break statement within the inner loop. This will interrupt the inner loop and allow the outer loop to continue executing. 
+
+//Q6. What is the difference between the break statement and the continue directive?
+//The break statement in JavaScript is used to terminate a loop immediately, exiting the loop and starting the execution of the code outside the loop. The continue directive, on the other hand, is used to skip the current iteration of the loop and move to the next iteration.
+
+//Q7. Can you stop a loop based on a specific condition?
+
+//Yes, we can stop a loop based on a specific condition by using control flow statements within the loop. The most common way to do this is by using the break statement
